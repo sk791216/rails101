2b289bb24c0d346d06eee334dd7f2df7e88d9671
meme <sk791216@gmail.com>
2019-03-25 00:30:06 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy, :join, :quit]
   before_action :find_group_and_check_permission , only: [:update, :edit, :destroy]
 
   def index
@@ -41,6 +41,29 @@ class GroupsController < ApplicationController
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
+  end
+  def join
+    @group= Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入討論區成功！"
+    else
+      flash[:warning] = "你已經是本討論板成員了"
+    end
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出討論版"
+    else
+      flash[:warning] = "你不是討論板成員,無法退出"
+    end
+    redirect_to group_path(@group)
   end
 
   private
